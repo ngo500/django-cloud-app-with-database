ffc5e8ffffdfb7b16e65569fd2962d748b117e15
Nikki Gowan <nikkigowan2@gmail.com>
2024-06-17 14:26:54 -0400

    Update models.py, Question

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -9,7 +9,6 @@ except Exception:
 from django.conf import settings
 import uuid
 
-
 # Instructor model
 class Instructor(models.Model):
     user = models.ForeignKey(
@@ -101,3 +100,21 @@ class Enrollment(models.Model):
 #class Submission(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
+
+# Question model
+class Question(models.Model):
+     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+     content = models.CharField(max_length=200)
+     grade = models.IntegerField(default=50)
+
+     def __str__(self):
+        return "Question: " + self.content
+    
+    def is_get_score(self, selected_ids):
+        all_answers - self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
